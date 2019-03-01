678a82589defbed14990e1be013b2ab4a763d7a9
Flavia Schaller <flaviaschaller@gmail.com>
2019-03-01 12:16:30 -0300

    Sourcing all images from folder

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link, graphql, StaticQuery } from 'gatsby'
+import { graphql, StaticQuery } from 'gatsby'
 import styled from 'styled-components'
 import Img from 'gatsby-image'
 
@@ -16,53 +16,13 @@ const Title = styled.h5`
 
 const Inner = styled.div`
     display: grid;
-    grid-template-columns: 1fr;
+    grid-template-columns: 1fr 1fr 1fr;
     grid-row-gap: 6rem;
 `
 
 const Box = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    column-gap: 5rem;
-    align-items: center;
-
-    ${Inner} &:nth-child(odd) {
-        grid-template-areas: 
-        "thumb text";
-    }
-
-    ${Inner} &:nth-child(even) {
-        grid-template-areas: 
-        "text thumb";
-    }
-
-    @media (max-width: 800px) {
-        grid-template-columns: 1fr;
-        grid-row-gap: 1rem;
-        grid-template-areas: none; 
-        ${Inner} &:nth-child(even),
-        ${Inner} &:nth-child(odd){
-        grid-template-areas: "thumb" "text"; 
-        }
-
-    }
-`
-
-const Thumb = styled(Link)`
     text-decoration: none;
-    color: inherit;
-    grid-area: thumb;
-
-`
-
-const Content = styled.div`
-    text-decoration: none;
-    grid-area: text;
-`
-
-const StyledLink = styled(Link)`
-    text-decoration: none;
-    color: inherit;
+  
 `
 
 const StyledImg = styled(Img)`
@@ -75,16 +35,6 @@ const StyledImg = styled(Img)`
         :hover {
             opacity: .7;
         }
-`
-
-const PostTitle = styled.h6`
-    margin-bottom: 0.5rem;
-
-`
-const Date = styled.p`  
-    font-size: 0.8rem;
-    display: block;
-    color: #777;
  
 `
 
@@ -95,25 +45,12 @@ export class Gallery extends Component {
         return (
             <Wrapper>
             <Title>
-                Works
+                Portfolio
             </Title>
                 <Inner>
-                {this.props.data.allMarkdownRemark.edges.map(({ node }) => (
+                {this.props.data.AllPostImages.edges.map(({ node }) => (
                     <Box key={node.id} className='box' >
-                        <Thumb to={node.fields.slug}>
-                            <StyledImg fluid={node.frontmatter.image.childImageSharp.fluid} />
-                        </Thumb> 
-                        <Content>
-                            <StyledLink to={node.fields.slug}>
-                                <PostTitle>
-                                    {node.frontmatter.title}{" "}
-                                </PostTitle>
-                            </StyledLink>
-                            <Date>
-                            {node.frontmatter.date}
-                            </Date>
-                            <p>{node.excerpt}</p>
-                        </Content>
+                        <StyledImg fluid={node.childImageSharp.fluid} />
                     </Box>
                 ))}
                 </Inner>
@@ -127,32 +64,21 @@ export default props => (
     <StaticQuery
     query={graphql`
       query {
-          allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-              totalCount
-              edges {
-              node {
-                  id
-                  frontmatter {
-                  title
-                  date(formatString: "DD MMMM, YYYY")
-                  image {
-                  childImageSharp {
-                      fluid(maxWidth: 800) {
-                      ...GatsbyImageSharpFluid_noBase64
-                        aspectRatio
-                      }
-                  }
-              }
-                  }
-                  fields {
-                  slug
-                  }
-                  excerpt
-              }
-              }
-          }
-          }
-    `}
+        AllPostImages: allFile(filter: { extension: { eq: "jpg" } }) {
+            edges {
+                node {
+                    id
+                    childImageSharp {
+                        fluid(maxWidth: 800) {
+                        ...GatsbyImageSharpFluid_noBase64
+                            aspectRatio
+                        }
+                     }
+                }
+            }
+        }
+    }
+`}
     render={data => <Gallery data={data} {...props} />}
     />
 )
